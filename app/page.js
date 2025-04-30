2d086a29bf8cc59968f159b841d8422ed7db2fc9
mohibnawaz <nawazsyed8402@gmail.com>
2025-04-30 11:28:18 +0530

    minor changes

--- app/page.js
+++ app/page.js
@@ -54,7 +54,7 @@ export default function Home() {
     const options = {
       method: "GET",
       headers: {
-        "x-rapidapi-key": "a3ecef6823msh7a112161d1b16cep1ef573jsndc3ab7a2a9dd",
+        "x-rapidapi-key": "e9ae821dcamsh17a3dd2ae614af3p18ef9bjsn41b31ab10379",
         "x-rapidapi-host": "fresh-linkedin-profile-data.p.rapidapi.com",
       },
     };
